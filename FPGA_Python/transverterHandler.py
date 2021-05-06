4c3ca66257a20338792ec4b25316a99a038e9958
dan <dan.m0wut@gmail.com>
2021-05-06 22:49:29 +0100

    Basic transverter discovery working

--- FPGA_Python/transverterHandler.py
+++ FPGA_Python/transverterHandler.py
@@ -1,15 +1,30 @@
 import serial
+import logging
+import json
 
 
 class TransverterHandler:
-    def __init__(self, filename, warnings):
+    def __init__(self, filename, warnings, numSlots=8):
         self.warnings = warnings
+        self.numSlots = numSlots  # @TODO Read from EEPROM
         try:
-            self.serial = serial.Serial(filename, 9600, timeout=1)
+            self.serial = serial.Serial(filename, 9600, timeout=0.1)
         except serial.serialutil.SerialException:
             self.warnings.add_error(
                 "Controller", "Transverter Control",
                 "Couldn't open serial port: {}".format(filename)
             )
+        self.run_discovery()
 
-        self.serial.write(b"Hello world")
+    def run_discovery(self):
+        # Query all the slots
+        for x in range(self.numSlots):
+            self.serial.write(x.to_bytes(1, 'big') + b"D\n")
+            response = self.serial.readline()
+            if response:
+                jsonResponse = json.loads(response.decode('utf-8'))
+                logging.info(
+                    "Transverter {} discovered in slot {}".format(
+                        jsonResponse['name'], x
+                    )
+                )
--- Transverter_Controller/Transverter_Controller/Transverter_Controller.ino
+++ Transverter_Controller/Transverter_Controller/Transverter_Controller.ino
@@ -1,24 +1,76 @@
+#include <ArduinoJson.h>
+
+
+#define LED 13
+#define ADDRESS 3
+#define DEBUG
+
+
+void process_command(String x){
+    if(x[0] != ADDRESS)
+        return; // First byte is address and it's not for us
+    else {
+        // Curently support:
+        // D = Discovery (static) info
+        // S = Status (changing) info
+        DynamicJsonDocument doc(200);
+
+        switch(x[1]){
+            case 'D': 
+                #ifdef DEBUG
+                    Serial.println("Received discovery request");
+                #endif
+                doc["name"] = "Bob" ;
+                doc["message"] = "Oh hai";
+                serializeJson(doc, Serial1);
+                break;
+            case 'S':
+                #ifdef DEBUG
+                    Serial.println("Received status request");
+                #endif
+                break;
+            default:
+                #ifdef DEBUG
+                    Serial.println("Received unknown command");
+                #endif
+                break;
+        }
+    }
+}
 
 void setup() {
     // initialize the digital pin as an output.
-    Serial.begin(9600);
     Serial1.begin(9600);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
+
+    #ifdef DEBUG
+        Serial.begin(9600);
+        while(!Serial); //wait for serial monitor
+    #endif
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+    
     static String command = "";
     while(1) {
+        digitalWrite(LED, LOW);
         while(!Serial1.available());
+        digitalWrite(LED, HIGH);
         char x = Serial1.read();
         if(x == '\n')
             break;
         else
             command += x;
     }
-    if(command != "")
-        Serial.println(command);
+    #ifdef DEBUG
+        if(command[0] == ADDRESS){
+            Serial.println(command);
+        }
+    #endif
 
+    process_command(command);
 
     command = "";
 }
